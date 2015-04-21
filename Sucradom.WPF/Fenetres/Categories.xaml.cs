27f2c063fe8be6cf0485b9c5a0d08892132825a2
Cyrille FAURE <faure.cyrille@free.fr>
2015-04-22 00:43:02 +0200

    IHM Categories
    Interfaces graphiques des categories
    +
    Modifications Dictionnaire Styles

--- Sucradom.WPF/Fenetres/Categories.xaml.cs
+++ Sucradom.WPF/Fenetres/Categories.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Sucradom.LIB;
 
 namespace Sucradom.WPF.Fenetres
 {
@@ -19,9 +20,32 @@ namespace Sucradom.WPF.Fenetres
     /// </summary>
     public partial class Categories : Window
     {
+
+        private Categories_ViewModel _ViewModel;
+
+
         public Categories()
         {
+            DataContext = _ViewModel = new Categories_ViewModel();
+
             InitializeComponent();
         }
+
+        #region Buttons_Clicks
+        private void Button_Insert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
     }
 }
--- Sucradom.WPF/Fenetres/Categories_ViewModel.cs
+++ Sucradom.WPF/Fenetres/Categories_ViewModel.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sucradom.LIB;
 
 namespace Sucradom.WPF.Fenetres
 {
-    class Categories_ViewModel : ViewModel
+    public class Categories_ViewModel : ViewModel
     {
+        private categorie _SelectedCategorie;
+        public categorie SelectedCategorie
+        {
+            get { return _SelectedCategorie; }
+            set 
+            {
+                _SelectedCategorie = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public Categories_ViewModel()
+        {
+
+        }
+
+        public void AjouterCategorie()
+        {   //********
+            // PENSER A AJOUTER LE CONSTRUCTEUR D'INSERTION
+            //new Formulaires.Categorie().ShowDialog();
+        }
+
+        public Boolean ModifierCategorie()
+        {
+            if (SelectedCategorie != null)
+            {
+                //********
+                // PENSER A AJOUTER LE CONSTRUCTEUR DE MODIFICATION
+                //new Formulaires.Categorie(SelectedCategorie).ShowDialog();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// ¨PENSER A FAIRE LES ETAPES DE SUPRESSIONS
+        /// </summary>
+        /// <returns></returns>
+        public Boolean SupprimerCategorie()
+        {
+            if (SelectedCategorie != null)
+            {
+                
+                return true;
+            }
+            return false;
+        }
     }
 }
