57066819f37cad036bac691900a6e01a8d99f971
jbblois <jb.blois@iia-laval.fr>
2015-04-21 20:08:35 +0200

    Mise en place du projet

---
+++ Sucradom.LIB/Classses/Base.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sucradom.LIB
+{
+    public static class Base
+    {
+        #region Connexion a la BDD
+        /// <summary>
+        /// Le contexte représente la BDD
+        /// </summary>
+        public static sucradomEntities Context { get; private set; }
+
+        /// <summary>
+        /// Permet d'ouvrir une connexion avec la BDD
+        /// </summary>
+        /// <returns>retourne true si l'action c'est bien effectuée</returns>
+        public static Boolean Debut()
+        {
+            Context = new sucradomEntities();
+            if (Context != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Permet de sauvegarder les modifications apportées à la DB
+        /// </summary>
+        /// <returns>retourne true si l'action c'est bien effectuée</returns>
+        public static Boolean Sauvegarder()
+        {
+            try
+            {
+                Context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Permet d'annuler les modifications apportées à la DB
+        /// </summary>
+        /// <returns>retourne true si l'action c'est bien effectuée</returns>
+        public static Boolean Annuler()
+        {
+            try
+            {
+                Context.Dispose();
+                Context = new sucradomEntities();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Permet de fermer la connexion a la BDD
+        /// </summary>
+        /// <returns>retourne true si l'action c'est bien effectuée</returns>
+        public static Boolean Fin()
+        {
+            try
+            {
+                Context.Dispose();
+                Context = null;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        #endregion
+
+        #region Variables Globales
+        #endregion
+
+    }
+}
---
+++ Sucradom.LIB/Classses/ClassesPartielles.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sucradom.LIB
+{
+   public partial class client
+   {
+       /// <summary>
+       /// Conversion du pseudo Booleen IsActif en Boolean
+       /// </summary>
+       public Boolean EstActif 
+       { 
+           get 
+           {
+               if (IsActif != null)
+               {
+                   // si IsActif == OUI alors .equals() retourne true
+                   // sinon IsActif == NON alors .equals() retourne false
+                    return IsActif.Equals("OUI");
+               }
+               else
+               {
+                   return false;
+               }
+           }
+           set
+           {
+               //Condition ternaire
+               //Variable = ( Condition ? 'ResultatTRUE' : 'ResultatFALSE' )
+               IsActif = (value ? "OUI" : "NON");
+           }
+       }
+
+       /// <summary>
+       /// Permet de récupérer le nom complet d'un client
+       /// </summary>
+       public String NomComplet { get { return Nom.ToUpper() + " " + Prenom; } }
+
+       /// <summary>
+       /// Initialiser un mot de passe pour le client
+       /// </summary>
+       public void GenererMDP()
+       { 
+           MotDePasse =  Prenom.First() + "" + Nom.ToLower() + "" + DateTime.Now.DayOfYear; 
+       }
+   }
+   public partial class produit
+   {
+       /// <summary>
+       /// Conversion du pseudo Booleen IsDisponible en Boolean
+       /// </summary>
+       public Boolean EstDisponible
+       {
+           get
+           {
+               if (IsDisponible != null)
+               {
+
+                   if (Quantite > 0)
+                   {
+                       // si IsDisponible == OUI alors .equals() retourne true
+                       // sinon IsDisponible == NON alors .equals() retourne false
+                       return IsDisponible.Equals("OUI");
+                   }
+                   else
+                   {
+                       IsDisponible = "NON";
+                   }
+               }
+                       return false;
+           }
+           set
+           {
+               if (Quantite > 0)
+               {
+                   //Condition ternaire
+                   //Variable = ( Condition ? 'ResultatTRUE' : 'ResultatFALSE' )
+                   IsDisponible = (value ? "OUI" : "NON");
+               }
+               else
+               {
+                   IsDisponible = "NON";
+               }
+           }
+       }
+
+   }
+
+   public partial class categorie
+   {
+       /// <summary>
+       /// Compte le nombre de produits de cette catégorie
+       /// </summary>
+       public int NbProduits 
+       { 
+           get
+           {
+               return produits.Count();
+           }
+       
+       }
+   }
+   public partial class taxe
+   {
+       /// <summary>
+       /// Compte le nombre de produits qui ont cette taxe
+       /// </summary>
+       public int NbProduits
+       {
+           get
+           {
+               return produits.Count();
+           }
+
+       }
+   }
+
+   public partial class adresse
+   {
+       public String NomP1
+       {
+           get
+           {
+               return Numero + ", rue " + Rue;
+           }
+
+       }
+       public String NomP2
+       {
+           get
+           {
+               return Ville + ", " + Cp.ToUpper();
+           }
+
+       }
+   }
+   public partial class provision
+   {
+       public String Nom
+       {
+           get
+           {
+               return Date.ToShortDateString() + " : " + Quantite;
+           }
+
+       }
+   }
+}
