a390233dd0a03e8db6f9f0e426ab993e958679e2
jbblois <jb.blois@iia-laval.fr>
2015-05-22 17:23:28 +0200

    Gestion des commandes partie 1

--- SucradomWEB/src/java/sucradom/metier/LigneCommande.java
+++ SucradomWEB/src/java/sucradom/metier/LigneCommande.java
@@ -17,12 +17,17 @@ public class LigneCommande
     public float PrixUnitaire;
     public float ValeurTaxe;
 
-    public LigneCommande(TeteCommande Commande, Produit Produit, int Quantite, float PrixTTC, float ValeurTaxe)
+    public LigneCommande(TeteCommande Commande, Produit Produit, int Quantite, float PrixUnitaire, float ValeurTaxe)
     {
         this.Commande = Commande;
         this.Produit = Produit;
         this.Quantite = Quantite;
         this.PrixUnitaire = PrixUnitaire;
         this.ValeurTaxe = ValeurTaxe;
+    }
+    
+    public float GetPrixTTC()
+    {
+        return Quantite * PrixUnitaire * ((ValeurTaxe/100)+1);
     }
 }
